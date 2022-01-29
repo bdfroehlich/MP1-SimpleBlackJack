9ba8afde693a25d191909f5596ec3ec6a5914b2b
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-01-29 15:51:55 -0600

    created Card and Deck classes, Deck class has a createDeck function which is called and converted from an object to an array of cards

---
+++ createDeck.js
@@ -0,0 +1,74 @@
+//set up card value array, suit array and empty deck class to push card objects into
+
+let cardValue = [2,3,4,5,6,7,8,9,10,'Jack','Queen','King','Ace'];
+
+let cardSuit = ['Clubs','Diamonds','Hearts','Spades'];
+
+class Card {
+    constructor(value, suit) {
+        this.value = value;
+        this.suit = suit;
+    }
+}
+
+class Deck {
+    constructor(){
+        this.deck = [];
+    }
+
+    createDeck(cardValue,cardSuit){
+        for(let value of cardValue){
+            for(let suit of cardSuit){
+                this.deck.push(new Card(value, suit))
+            }
+        }
+        return this.deck;
+    }
+}
+
+
+// let testCard = new Card(2,'Clubs');
+// console.log(testCard)
+
+
+const deck = new Deck();
+deck.createDeck(cardValue,cardSuit)
+console.log(deck)
+
+//convert deck object to an array
+const deckArray = Object.values(deck)
+console.log(deckArray)
+
+//insert key-value pair conditionally to the deckArray
+
+
+
+// function weightedDeck(array) {array.map(element => {
+//         if(typeof(element.value) == 'number') {
+//                 element.weightvalue = element.value;
+//         } else if (element.value == 'Jack' || element.value == 'Queen' || element.value == 'King') {
+//                 element.weightvalue = 10;
+//         } else if (element.value == 'Ace'){
+//                 element.weightvalue = 11;
+//         }
+// })
+// };
+
+
+
+
+
+
+// function addCardValue(array) {
+//     array.map(element => {
+//             if(typeof(element.value) == 'number') {
+//                 element.weightvalue = element.value;
+//             } else if (element.value == 'Jack' || element.value == 'Queen' || element.value == 'King') {
+//                 element.weightvalue = 10;
+//             } else if (element.value == 'Ace'){
+//                 element.weightvalue = 11;
+//             }
+//     })
+// }
+
+// console.log(addCardValue(deck))
