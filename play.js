2b136dbff3dca7388f43e0ee446ff90f528c5196
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-01-31 19:36:39 -0600

    created dummy deal functions, tested card image rendering

--- play.js
+++ play.js
@@ -11,3 +11,32 @@ deck.cards.map(element =>{
 // console.log(playQueue.dequeue())
 // console.log(playQueue.dequeue())
 console.log(getCardImageName(playQueue.dequeue()))
+
+function firstPlayerCard(){
+    //create image element associated with first card off top of deck
+    // var playerCard = getCardImageName(playQueue.dequeue());
+    var createImage = document.createElement("img");
+    createImage.src = "assets/3_of_hearts.png";
+    document.body.appendChild(createImage);
+
+}
+
+function firstDealerCard(){
+    //create image element associated with first card off top of deck
+}
+
+function secondPlayerCard(){
+    //create image element associated with first card off top of deck
+}
+
+function secondDealerCard(){
+    //create image element associated with first card off top of deck
+}
+
+function testCardImage(){
+    for(let i=0; i<=10; i++){
+        firstPlayerCard()
+    }
+}
+
+testCardImage()
