b85bb22af1a206d7ab6d1018b12b897bba897969
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-02-01 21:24:34 -0600

    updated existing functions, added refreshdeck and cleardeck

--- play.js
+++ play.js
@@ -1,59 +1,81 @@
 //***variables***
-const deck = new Deck()
-deck.shuffleDeck()
+var deck = new Deck();
+deck.shuffleDeck();
 var isDealClicked = false;
+
+let playerArray = [];
+let dealerArray = [];
 // console.log(deck.cards)
 
+const dealerCards = document.querySelector(".dealer-cards");
+const playerCards = document.querySelector(".player-cards");
+
+
 //***create new Queue to put our shuffled array of cards into a playQueue***
-let playQueue = new Queue()
-deck.cards.map(element =>{
-    playQueue.enqueue(element)
-})
-
-// console.log(playQueue.dequeue())
-// console.log(getCardImageName(playQueue.dequeue()))
-
+// let playQueue = new Queue();
+// deck.cards.map(element =>{
+//     playQueue.enqueue(element)
+// })
 
 //***deal card & image render functions***
-async function firstPlayerCard(){
+function dealPlayerCard(){
     //create image element associated with first card off top of deck
     var createImage = document.createElement("img");
-    createImage.src = getCardImageName(playQueue.dequeue());
+    createImage.src = getPlayerCard(drawTopCard());
     document.querySelector('.player-cards').appendChild(createImage);
 }
 
-function firstDealerCard(){
+function dealDealerCard(cardIsFaceUp = true){
     var createImage = document.createElement("img");
-    createImage.src = getCardImageName(playQueue.dequeue());
+    var card = drawTopCard();
+    card.cardIsFaceUp = cardIsFaceUp;
+    createImage.src = getDealerCard(card);
     document.querySelector('.dealer-cards').appendChild(createImage);
 }
 
-async function secondPlayerCard(){
+function hitCard(){
     var createImage = document.createElement("img");
-    createImage.src = getCardImageName(playQueue.dequeue());
+    createImage.src = getPlayerCard(drawTopCard());
     document.querySelector('.player-cards').appendChild(createImage);
 }
 
-function secondDealerCard(){
-    var createImage = document.createElement("img");
-    createImage.src = getCardImageName(playQueue.dequeue());
-    document.querySelector('.dealer-cards').appendChild(createImage);
+function stand(){
+
+}
+
+function clearTable(){
+    dealerCards.innerHTML = "";
+    playerCards.innerHTML = "";
+
+    refreshDeck();
+    playerArray = [];
+    dealerArray = [];
+}
+
+function refreshDeck(){
+    deck = new Deck();
+    deck.shuffleDeck();
+}
+
+var dealCards = function(){
+    dealPlayerCard()
+    dealDealerCard(false)
+    dealPlayerCard()
+    dealDealerCard()
+    if(isDealClicked == false) { 
+        this.removeEventListener("click", dealCards);
+    }
 }
 
 //***handlers***
-document.querySelector('.deal').addEventListener('click', () =>{
-    firstPlayerCard()
-    firstDealerCard()
-    secondPlayerCard()
-    secondDealerCard()
-    console.log(playQueue)
+document.querySelector('.deal').addEventListener("click", dealCards)
+document.querySelector('.hit').addEventListener("click", hitCard)
+document.querySelector('.newround').addEventListener("click", function(){
+    document.querySelector('.deal').addEventListener("click", dealCards)
+    clearTable()
+
 })
 
 
-
-// firstPlayerCard()
-// firstDealerCard()
-// secondPlayerCard()
-// secondDealerCard()
 // playQueue.dequeue()
 // console.log(playQueue)
